6b385f81106cfdf7b96426c1644d3ad2ff2994ee
Vlad Kukovsky <brodsky@MacBook-Air-Vlad.local>
2017-09-29 11:05:52 +0300

    Initial Commit

---
+++ long ar/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  long ar
+//
+//  Created by Vlad Kukovsky on 29.09.17.
+//  Copyright © 2017 Ku. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
